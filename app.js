d239988df352881d6c71a66b59b273dd746ba5fd
Melih00 <melihagilli0@gmail.com>
2021-07-09 00:07:07 +0300

    Time to sleep

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-function noExist() {
-  alert(
-    "Bu içeriği şu anda görüntüleme izniniz yok.\nDetaylı bilgi için  --->https://bit.ly/3e0BNqi"
-  );
-}
-function cantEnterUser() {
-  alert(
-    "Şu anda bu kişiye geçemezsiniz.\nDetaylı bilgi için  --->https://bit.ly/3e0BNqi"
-  );
-}
-function cantEnterPage() {
-  alert(
-    "Şu anda bu sayfayı görüntüleme izniniz yok.\nDetaylı bilgi için ---> " +
-      redirURL +
-      "https://bit.ly/3e0BNqi"
-  );
-}
-
 $(function () {
   $("#dialog").dialog({
     autoOpen: false,
